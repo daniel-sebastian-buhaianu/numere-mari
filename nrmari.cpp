b4fe62a560241dc2834a9bb176f84455476f28a5
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-10-17 09:15:58 +0300

    implementeaza adunaNumereMari()

--- nrmari.cpp
+++ nrmari.cpp
@@ -5,12 +5,15 @@ using namespace std;
 typedef char NrMare[NRMAX_CIFRE];
 void citesteNumarMare(NrMare, int &);
 void afiseazaNumarMare(NrMare, int);
+void adunaNumereMari(NrMare, int, NrMare, int, NrMare, int &);
 int main()
 {
-	NrMare x;
-	int nx;
-	citesteNumarMare(x, nx);
-	afiseazaNumarMare(x, nx);
+	NrMare a, b, s;
+	int na, nb, ns;
+	citesteNumarMare(a, na);
+	citesteNumarMare(b, nb);
+	adunaNumereMari(a, na, b, nb, s, ns);
+	afiseazaNumarMare(s, ns);
 	return 0;
 }
 void citesteNumarMare(NrMare nr, int & nrcif)
@@ -36,4 +39,35 @@ void afiseazaNumarMare(NrMare nr, int nrcif)
 	}
 	cout << '\n';
 }
-
+void adunaNumereMari(NrMare a, int na, NrMare b, int nb, NrMare s, int & ns)
+{
+	int i, t, cif;
+	// completeaza numarul cel mai mic cu zerouri nesemnificative
+	if (na < nb)
+	{
+		for (i = na; i < nb; i++)
+		{
+			a[i] = 0;
+		}
+		ns = nb;
+	}
+	else
+	{
+		for (i = nb; i < na; i++)
+		{
+			b[i] = 0;
+		}
+		ns = na;
+	}
+	// aduna cele doua numere si retine rezultatul in s si ns
+	for (t = i = 0; i < ns; i++)
+	{
+		cif = a[i]+b[i]+t;
+		s[i] = cif%10;
+		t = cif/10;
+	}
+	if (t)
+	{
+		s[ns++] = t;
+	}
+}
